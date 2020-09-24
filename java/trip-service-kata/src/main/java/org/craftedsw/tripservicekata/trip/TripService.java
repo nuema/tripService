a7e92d409e4cce918780332dd78264584124322f
Natalia Uema <nuema@Alfonsina.local>
2020-09-24 09:49:38 -0300

    first commit

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,32 +1,48 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
+import static java.util.Collections.emptyList;
+
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
-	
+    private Supplier<User> getLoggedUser;
+    private Function<User, List<Trip>> findTripsByUser;
+
+    public TripService() {
+        getLoggedUser = UserSession.getInstance()::getLoggedUser;
+        findTripsByUser = TripDAO::findTripsByUser;
+    }
+
+    public TripService(Supplier<User> getLoggedUser) {
+        this.getLoggedUser = getLoggedUser;
+    }
+
+    public TripService setFindTripsByUser(Function<User, List<Trip>> findTripsByUser) {
+        this.findTripsByUser = findTripsByUser;
+        return this;
+    }
+
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        return areFriends(user, getLoggedUser()) ?
+                findTripsByUser.apply(user) : emptyList();
+    }
+
+    private User getLoggedUser() {
+        User loggedUser = getLoggedUser.get();
+        if (loggedUser == null)
+            throw new UserNotLoggedInException();
+
+        return loggedUser;
+    }
+
+    private boolean areFriends(User user, User otherUser) {
+        return user.getFriends().contains(otherUser);
+    }
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,51 @@
 package org.craftedsw.tripservicekata.trip;
 
+
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+import org.craftedsw.tripservicekata.exception.*;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class TripServiceTest {
-	
+
+    TripService tripService;
+
+    @Test
+    public void shouldFailWhenUserIsNotLogged() {
+        tripService = new TripService(() -> null);
+
+        Assertions.assertThrows(UserNotLoggedInException.class,
+                () -> tripService.getTripsByUser(new User()));
+    }
+
+    @Test
+    void shouldReturnNoTrips() {
+        tripService = new TripService(User::new);
+        User notAFriend = new User();
+
+        Assertions.assertTrue(tripService.getTripsByUser(notAFriend).isEmpty());
+    }
+
+    @Test
+    void shouldReturnTrips() {
+
+        //Given
+        User loggedUser = new User();
+        User aFriend = new User();
+        aFriend.addFriend(loggedUser);
+
+        List<Trip> trips = new ArrayList<>();
+        trips.add(new Trip());
+
+        tripService = new TripService(() -> loggedUser)
+                .setFindTripsByUser((user) -> trips);
+
+        //When
+        Assertions.assertTrue(!tripService.getTripsByUser(aFriend).isEmpty());
+    }
 }
